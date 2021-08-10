da555d54408b572e2dd33f92a5b41c9048298084
Yang Xi Gao <joyce.gao01@gmail.com>
2021-08-10 14:46:55 +0000

    removed strapi testing to different repo

--- strapi/fetch.rb
+++
@@ -1,6 +0,0 @@
-require 'httparty'  # gem install httparty
-require 'json'      # gem install json
-
-response = HTTParty.get('https://strapifeather.ngrok.io/contents')
-jsontext= JSON.parse(response.body)
-File.write('./content.json', JSON.dump(jsontext))
--- strapi/posts.js
+++
@@ -1,6 +0,0 @@
-//var Mustache = require('mustache');
-
-var data = JSON.parse(fs.readFileSync('./content.json'));
-
-//var output = Mustache.render('{{Title}}', data);
-console.log(data);
