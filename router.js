b53df032c66a82f8d9a4ee0280df9a47c8afbc19
Yang Xi Gao <joyce.gao01@gmail.com>
2021-08-20 20:35:35 +0000

    mixed smooth scrolling/routing issue

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const pages = document.getElementsByClassName("page");
+var currPage;
 
 // if there is no hash, default to main hash
 if(window.location.hash == ""){
     window.location.hash = "main";
+    currPage = 'main';
 } else {
     onRouteChanged()
 }
@@ -11,21 +13,27 @@ if(window.location.hash == ""){
 function onRouteChanged() {
     var hash = window.location.hash.split('#')[1];
 
-    // temporary fix
-    if (hash == "cta_container") {
-        for (page of pages) {
-            page.style.display = (page.id == 'main' ? "block" : "none");
-        }
-        return;
-    }
-
     for (page of pages) {
         page.style.display = (page.id == hash ? "block" : "none");
     }
     if (hash != "main"){
         window.scrollTo(0,0);
     }
-    
+
+    currPage = hash;
+}
+
+// smooth scrolls to elements on main page
+function mainSmoothScroll(element_id){
+
+    if (currPage != "main"){
+        for (page of pages) {
+            page.style.display = (page.id == 'main' ? "block" : "none");
+        }
+        currPage = "main";
+    }
+
+    document.getElementById(element_id).scrollIntoView({behavior: "smooth"});
 }
 
 window.addEventListener("hashchange", onRouteChanged);
