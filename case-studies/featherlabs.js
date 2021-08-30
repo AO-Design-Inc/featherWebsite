3aa003f4936bfb3bcab3a9d789a07d4859753742
Yang Xi Gao <joyce.gao01@gmail.com>
2021-08-30 13:04:55 +0000

    push fetched generated files for deployment

---
+++ case-studies/featherlabs.js
@@ -0,0 +1,145 @@
+let featherlabsString = `
+<template id="featherlabs">
+    <dash-board action="email"></dash-board>
+    <div id="splash" class="image-cover" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%), 
+        url('./case-studies/assets/thumbnails/labs_splash.png.webp');">
+        <div id="splash-text">
+            <p class="splash-year">2021</p>
+            <p class="splash-org">Design</p>
+            <p class="splash-title">Feather Labs</p>
+            <down-button onclick="smoothScrollOneFrameDown()"></down-button>
+        </div>
+    </div>
+    <div id="section-container">
+        <div class="section">
+
+            <p class="section-title">1 Introduction</p>
+
+        
+            <div class="section-content">
+                <p class="section-heading">
+                    Minimalistic 3D for the Web
+                </p>
+
+                <p class="section-text">As part of an effort to create experimental, cutting edge web graphics, we made Feather Labs. A space for us to serve and demo cutting edge web experiments and art. Labs&#39; first project can be found here: [https://labs.feather.systems/](https://labs.feather.systems/). It is an MVP demo of a fast, performant 3D scene on the web.</p>
+            </div>
+
+                <img class="section-image" src="./case-studies/assets/images/labs_content_4.png.webp"/>
+
+
+        </div>
+        <div class="section">
+
+            <p class="section-title">2 Challenge</p>
+
+        
+            <div class="section-content">
+                <p class="section-heading">
+                    The Web is Finicky &amp; Difficult to Work With
+                </p>
+
+                <p class="section-text">Rendering 3D graphics on the web is hard. Rendering 3D graphics without huge dependencies and slow load times is even harder. Figuring out how to make an aesthetically coherent site with our minimalistic approach to web development was definitely a challenge. We effectively communicated the advantages of our site through real time calculated stats to show that we practice what we preach. You can achieve a rich website experience with a minimalistic approach to development.</p>
+            </div>
+
+                <img class="section-image" src="./case-studies/assets/images/labs_content_1.png.webp"/>
+
+
+        </div>
+        <div class="section">
+
+            <p class="section-title">3 Process</p>
+
+        
+            <div class="section-content">
+                <p class="section-heading">
+                    The Iterative Process is our God
+                </p>
+
+                <p class="section-text">We needed an MVP to demonstrate our capabilities in a succinct project. To keep the design/development process quick we sprinted through a bunch of layout &amp; illustration iterations while developing workflows to efficiently execute complex web graphics in parallel. Our minimalistic approach to both design &amp; development made it easy to make fast changes.</p>
+            </div>
+
+                <img class="section-image" src="./case-studies/assets/images/labs_content_2.png.webp"/>
+
+
+        </div>
+        <div class="section">
+
+
+        
+            <div class="section-content">
+                <p class="section-heading">
+</p>
+
+                <p class="section-text"></p>
+            </div>
+
+                <img class="section-image" src="./case-studies/assets/images/labs_content_3.png.webp"/>
+
+
+        </div>
+        <div class="section">
+
+            <p class="section-title">4 Results</p>
+
+        
+            <div class="section-content">
+                <p class="section-heading">
+                    Fast &amp; Fabulous
+                </p>
+
+                <p class="section-text">We have successfully made a lightweight website (transferring only ~140kb over the wire) with cutting edge web graphics and a coherent visual style. In the future, we will be serving new experiments and innovations at this page. Keep a look out for more fun web experiments!</p>
+            </div>
+
+
+            <hr>
+
+        </div>
+    </div>
+
+    <div id="footer">
+        <div class="footer-banner">
+            <div class="footer-text">
+                <div class="logo">
+                    <svg viewBox="0 0 339 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M71 53.2204H82.2198V31.2358H103.804V21.9586H82.2198V9.27719H106.136V0H71V53.2204Z" fill="#11131C"/>
+                        <path d="M128.693 54C138.539 54 145.173 49.1925 146.727 41.7863L136.518 41.1107C135.404 44.1511 132.554 45.7363 128.874 45.7363C123.355 45.7363 119.857 42.0722 119.857 36.1213V36.0953H146.961V33.0549C146.961 19.4899 138.773 12.7854 128.252 12.7854C116.54 12.7854 108.948 21.127 108.948 33.4447C108.948 46.1001 116.436 54 128.693 54ZM119.857 29.2348C120.09 24.6872 123.536 21.0491 128.434 21.0491C133.227 21.0491 136.544 24.4793 136.57 29.2348H119.857Z" fill="#11131C"/>
+                        <path d="M165.514 53.974C171.396 53.974 175.205 51.4013 177.148 47.6853H177.459V53.2204H187.927V26.2984C187.927 16.7873 179.895 12.7854 171.033 12.7854C161.497 12.7854 155.227 17.359 153.698 24.6352L163.907 25.4668C164.658 22.8162 167.016 20.8672 170.981 20.8672C174.738 20.8672 176.889 22.7642 176.889 26.0385V26.1944C176.889 28.7671 174.168 29.1049 167.25 29.7806C159.372 30.5082 152.298 33.1588 152.298 42.0722C152.298 49.9721 157.921 53.974 165.514 53.974ZM168.675 46.334C165.28 46.334 162.845 44.7488 162.845 41.7084C162.845 38.59 165.41 37.0568 169.297 36.5111C171.706 36.1732 175.645 35.6015 176.967 34.718V38.9538C176.967 43.1376 173.52 46.334 168.675 46.334Z" fill="#11131C"/>
+                        <path d="M217.661 13.3051H210.173V3.74206H199.134V13.3051H193.693V21.6208H199.134V42.41C199.082 50.2319 204.394 54.1039 212.401 53.7661C215.251 53.6622 217.273 53.0905 218.387 52.7267L216.651 44.4889C216.106 44.5929 214.94 44.8527 213.904 44.8527C211.701 44.8527 210.173 44.0212 210.173 40.9548V21.6208H217.661V13.3051Z" fill="#11131C"/>
+                        <path d="M236.745 30.1444C236.771 24.999 239.907 21.9846 244.415 21.9846C248.898 21.9846 251.541 24.8691 251.515 29.7806V53.2204H262.554V27.8056C262.579 18.4504 257.086 12.7854 248.794 12.7854C242.757 12.7854 238.715 15.6439 236.901 20.3474H236.434V0H225.707V53.2204H236.745V30.1444Z" fill="#11131C"/>
+                        <path d="M289.379 54C299.225 54 305.859 49.1925 307.413 41.7863L297.204 41.1107C296.09 44.1511 293.24 45.7363 289.56 45.7363C284.041 45.7363 280.543 42.0722 280.543 36.1213V36.0953H307.647V33.0549C307.647 19.4899 299.458 12.7854 288.938 12.7854C277.226 12.7854 269.634 21.127 269.634 33.4447C269.634 46.1001 277.122 54 289.379 54ZM280.543 29.2348C280.776 24.6872 284.222 21.0491 289.12 21.0491C293.913 21.0491 297.23 24.4793 297.256 29.2348H280.543Z" fill="#11131C"/>
+                        <path d="M314.85 53.2204H325.889V30.6381C325.889 25.7267 329.464 22.3484 334.336 22.3484C335.865 22.3484 337.964 22.6083 339 22.9461V13.1232C338.015 12.8893 336.642 12.7334 335.528 12.7334C331.071 12.7334 327.417 15.332 325.966 20.2695H325.552V13.3051H314.85V53.2204Z" fill="#11131C"/>
+                        <path d="M15.1541 0C-0.0952275 0 1.44967e-05 14.7825 1.44967e-05 14.7825V23.2276C1.44967e-05 23.2276 3.47177 14.7825 13.0845 14.7825H14.0175H45.4621C60.9598 14.7825 60.6189 0.161148 60.6153 0.0238174H60.5785L60.6161 0.0007726H52.5404H32.9393L15.1541 0ZM15.1541 18.4789C-0.0952275 18.4789 1.44967e-05 33.2614 1.44967e-05 33.2614V41.7065C1.44967e-05 41.7065 3.47175 33.2614 13.0845 33.2614H14.0175H45.4621C60.9598 33.2614 60.6189 18.64 60.6153 18.5027H60.5785L60.6161 18.4797H52.5404C52.5404 18.4797 39.1703 18.4798 32.9393 18.4797C26.7082 18.4795 15.1541 18.4789 15.1541 18.4789V18.4789Z" fill="#11131C"/>
+                        <path d="M15.1541 36.9463C-0.0952139 36.9463 1.44905e-05 51.729 1.44905e-05 51.729H17.806H18.1849C33.6826 51.729 33.3417 37.1074 33.3381 36.9701H33.3012L33.3388 36.9471H25.2631H15.1333L15.1541 36.9463Z" fill="#11131C"/>
+                    </svg>
+                </div>
+                <p class="logo-subtext">Beautiful, lightweight websites</p>
+
+                <feather-button text="Contact Us" onclick="location.href='#main#cta-screencell';"></feather-button>
+            </div>
+        </div>
+
+    </div>
+</template>
+`;
+
+
+class featherlabsPage extends HTMLElement{
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        var parser = new DOMParser();
+
+        // fetch html from file
+        var templateHtml = parser.parseFromString(featherlabsString, 'text/html');
+
+        // get the template contents
+        var template = templateHtml.getElementById('featherlabs');
+        var content = template.content;
+        
+        this.appendChild(content);
+    }
+}
+
+customElements.define('featherlabs-page', featherlabsPage);
