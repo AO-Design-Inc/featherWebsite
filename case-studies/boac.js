f7965de1d8b5376fdea88c6541e2e5d218fd8c50
Yang Xi Gao <joyce.gao01@gmail.com>
2021-08-30 18:07:01 +0000

    fixed tiling

--- case-studies/boac.js
+++ case-studies/boac.js
@@ -147,7 +147,7 @@ Our strategy required a two pronged approach. First we transformed the root doma
         </div>
 
         <div class="next-study image-cover" class="image-cover"
-                style="background: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%), 
+                style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%), 
                     url('./case-studies/assets/thumbnails/varuna-splash.png.webp');">
             <div class="next-study-container">
                 <h3>NEXT CASE STUDY</h3>
--- case-studies/featherlabs.js
+++ case-studies/featherlabs.js
@@ -119,7 +119,7 @@ let featherlabsString = `
         </div>
 
         <div class="next-study image-cover" class="image-cover"
-                style="background: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%), 
+                style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%), 
                     url('./case-studies/assets/thumbnails/cover.png.webp');">
             <div class="next-study-container">
                 <h3>NEXT CASE STUDY</h3>
